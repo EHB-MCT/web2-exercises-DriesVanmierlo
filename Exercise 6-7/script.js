41cc37fdf260a10dc427de143ec3c5ee66e12547
DriesVanmierlo <dries.vanmierlo@student.ehb.be>
2021-10-02 15:36:37 +0200

    Finished exercise 6

--- Exercise 6-7/script.js
+++ Exercise 6-7/script.js
@@ -1,20 +1,45 @@
 'Use strict';
 
+const dishes = [{
+        id: '1',
+        name: "Kapsalon",
+        price: '8'
+    }, {
+        id: '2',
+        name: "Ribbetjes met frieten",
+        price: '16'
+    },
+    {
+        id: '3',
+        name: "Burger",
+        price: '12'
+    }
+];
+
 window.onload = function () {
     console.log("document loaded");
 
     const form = document.getElementById('form').addEventListener('submit', (e) => {
         e.preventDefault("submit");
+
+
+
         let orderdetails = {
             name: document.getElementById("nameForm").value,
             email: document.getElementById("emailForm").value,
-            order: document.getElementById("orderForm").value
+            order: document.querySelector('input[name="dish"]:checked').value
         }
+
+        let dish = dishes.find(d => d.id == orderdetails.order);
+
+        orderdetails.order = dish;
+
+        console.log(orderdetails.order.name);
 
         printOrder(orderdetails);
     });
 }
 
 function printOrder(orderdetails) {
-    let message = document.getElementById("messages").innerHTML = `<p>The order for the customer ${orderdetails.name}  is the following:  ${orderdetails.order}. The customer may be notified by email:  ${orderdetails.email}</p>`
+    let message = document.getElementById("messages").innerHTML = `<p>The order for the customer ${orderdetails.name}  is the following:  ${orderdetails.order.name}. The customer may be notified by email:  ${orderdetails.email}</p>`
 }
