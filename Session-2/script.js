1423a33e045408ddd45d386ed346a042b76ea15c
DriesVanmierlo <dries.vanmierlo@student.ehb.be>
2021-10-05 16:06:22 +0200

    Completed step 1
    After each Pokémon, there is still a comma

--- Session-2/script.js
+++ Session-2/script.js
@@ -1,6 +1,8 @@
 'Use strict';
 
 let pokemonDataList = [];
+let pokemonHTML = "";
+let pokemonListHTML = [];
 
 fetchPokemon();
 
@@ -23,5 +25,36 @@ function fetchPokemon() {
                     });
             })
         })
-    console.log("LIJST:", pokemonDataList);
+    console.log("All pokemon:", pokemonDataList);
+
+}
+
+window.onload = function () {
+    setTimeout(buildList, 3000);
+
+    function buildList() {
+        pokemonDataList.forEach(e => {
+            pokemonHTML = `<article class="pokemonArticle">
+        <figure class="pokemonFigure">
+            <img src="${e.sprites.front_default}" alt="">
+        </figure>
+        <div class="pokemonInfo">
+            <h2>${e.name}</h2>
+            <div class="pokemonTypes">
+                <p>Type 1</p>
+                <p>Type 2</p>
+            </div>
+        </div>
+        <div class="addToTeamButton">
+            <button>Add to team</button>
+        </div>
+    </article>`
+
+            pokemonListHTML.push(pokemonHTML);
+        })
+
+        document.getElementById('sectionList').innerHTML = pokemonListHTML;
+
+
+    }
 }
